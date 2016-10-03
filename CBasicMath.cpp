28fbe860c319c55b802a27d3177d019ea4080577
Shiritoo <shirito72@gmail.com>
2016-10-03 22:27:14 +0200

    Init

---
+++ CBasicMath.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Arthur on 03/10/2016.
+//
+
+#include "CBasicMath.hpp"
+
+int CBasicMath::Addition(int x, int y)
+{
+    return (x + y);
+}
+
+int CBasicMath::Multiply(int x, int y)
+{
+    return (x * y);
+}
---
+++ CBasicMath.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Arthur on 03/10/2016.
+//
+
+#ifndef BASIC_MATH_HPP__
+#define BASIC_MATH_HPP__
+
+class CBasicMath
+{
+public:
+    int Addition(int x, int y);
+    int Multiply(int x, int y);
+};
+
+#endif
+
+#endif //TESTC_CBASICMATH_H
