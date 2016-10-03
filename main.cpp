b3ecc7b70858abde1fc113560172c4a67678e81c
Mangiang <Mangiang@users.noreply.github.com>
2016-10-03 22:54:19 +0200

    test2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
     cout    << "Hello, World!" << endl
-            << "Je build !" << endl;
+            << "Je build !" << endl
+           << "Ou pas!" << endl;;
     return 0;
 }
