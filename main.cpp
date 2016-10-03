d36b947013ae53f7d3c24eb95e381e36a571c582
Shiritoo <shirito72@gmail.com>
2016-10-03 22:59:01 +0200

    test3

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     cout    << "Hello, World!" << endl
             << "Je build !" << endl
-           << "Ou pas!" << endl;;
+           << "Ou pas!" << endl
+    << "Enfait si !" << endl;
     return 0;
 }
